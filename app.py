c12d7d3df586940803a01b0b6d7ffb99c9ea1161
arsummers <aliyasummers1@gmail.com>
2021-01-28 16:08:03 -0800

    boilerplate server

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 import os
 
 from slack_bolt import App
 
+app = App(
+    token = os.environ.get("SLACK_BOT_TOKEN"),
+    signing_secret = os.environ.get("SLACK_SIGNING_SECRET")
+)
+
+if __name__ == "__main__":
+    app.start(port=int(os.environ.get("PORT", 3000)))

ab0c9d3a688596ddf021136b33c049dfc2b6e3d7
arsummers <aliyasummers1@gmail.com>
2021-03-11 14:19:24 -0800

    ready to try sleep decorator

--- app.py
+++ app.py
@@ -106,6 +106,18 @@ def sleep(timeout, retry=3):
     def decorator(function):
       def wrapper(*args, **kwargs):
           retries = 0
+          while retries < retry:
+              try:
+                  value = function(*args, **kwargs)
+                  if value is None:
+                      return
+              except:
+                  print(f'Sleeping for {timeout} seconds')
+                  time.sleep(timeout)
+                  retries += 1
+
+      return wrapper
+  return decorator
 
 
 
